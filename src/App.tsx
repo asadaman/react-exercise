efa2ede38a2b196105608a7c85737c424bf38ff9
asadaman <asdm.dev@gmail.com>
2017-12-14 19:25:20 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,179 @@
+import * as React from 'react';
+import './App.css';
+
+const logo = require('./logo.svg');
+
+function Square(props: any) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+// interface  PropTypes {
+//   value: number
+// }
+
+// class Square extends React.Component<PropTypes, any> {
+//   render() {
+//     return (
+//       <button className="square">
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+// const Square2: React.SFC<PropTypes> = ({value}) => (
+//   <button className="square">
+//     {value}
+//   </button>
+// )
+
+class App extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      squares: Array(81).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i: number) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? '●' : '○';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  renderSquare(i: number) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <div className="background">
+        <div className="board">
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(9)}
+            {this.renderSquare(10)}
+            {this.renderSquare(11)}
+            {this.renderSquare(12)}
+            {this.renderSquare(13)}
+            {this.renderSquare(14)}
+            {this.renderSquare(15)}
+            {this.renderSquare(16)}
+            {this.renderSquare(17)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(18)}
+            {this.renderSquare(19)}
+            {this.renderSquare(20)}
+            {this.renderSquare(21)}
+            {this.renderSquare(22)}
+            {this.renderSquare(23)}
+            {this.renderSquare(24)}
+            {this.renderSquare(25)}
+            {this.renderSquare(26)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(27)}
+            {this.renderSquare(28)}
+            {this.renderSquare(29)}
+            {this.renderSquare(30)}
+            {this.renderSquare(31)}
+            {this.renderSquare(32)}
+            {this.renderSquare(33)}
+            {this.renderSquare(34)}
+            {this.renderSquare(35)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(36)}
+            {this.renderSquare(37)}
+            {this.renderSquare(38)}
+            {this.renderSquare(39)}
+            {this.renderSquare(40)}
+            {this.renderSquare(41)}
+            {this.renderSquare(42)}
+            {this.renderSquare(43)}
+            {this.renderSquare(44)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(45)}
+            {this.renderSquare(46)}
+            {this.renderSquare(47)}
+            {this.renderSquare(48)}
+            {this.renderSquare(49)}
+            {this.renderSquare(50)}
+            {this.renderSquare(51)}
+            {this.renderSquare(52)}
+            {this.renderSquare(53)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(54)}
+            {this.renderSquare(55)}
+            {this.renderSquare(56)}
+            {this.renderSquare(57)}
+            {this.renderSquare(58)}
+            {this.renderSquare(59)}
+            {this.renderSquare(60)}
+            {this.renderSquare(61)}
+            {this.renderSquare(62)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(63)}
+            {this.renderSquare(64)}
+            {this.renderSquare(65)}
+            {this.renderSquare(66)}
+            {this.renderSquare(67)}
+            {this.renderSquare(68)}
+            {this.renderSquare(69)}
+            {this.renderSquare(70)}
+            {this.renderSquare(71)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(72)}
+            {this.renderSquare(73)}
+            {this.renderSquare(74)}
+            {this.renderSquare(75)}
+            {this.renderSquare(76)}
+            {this.renderSquare(77)}
+            {this.renderSquare(78)}
+            {this.renderSquare(79)}
+            {this.renderSquare(80)}
+          </div>
+        </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
